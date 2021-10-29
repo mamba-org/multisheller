62471afd2116003f63dffae3f8fe9f4c4000670a
Silvio Traversaro <silvio@traversaro.it>
2021-10-29 16:03:01 +0200

    Complete multisheller cli tool
    The existing version of the multisheller tool just saved some files in
    the /tmp directory. This commit complete support for the tool.
    
    By default, the converted scripts are saved in the same directory
    of the input file, just changing the extension to the extension of the
    script. The user can specify a different output with the --output option.
    
    The tool now automatically creates a <output>.sh script that automatically
    calls the appropriate script (either <output>.bash or <output>.zsh) based
    on wheter the script is running under bash or zsh.
    See https://github.com/mamba-org/multisheller/issues/11 for more details.
    
    The README is extended with a basic example on the use of the cli tool.
    
    Fix https://github.com/mamba-org/multisheller/issues/15
    Fix https://github.com/mamba-org/multisheller/issues/11

--- multisheller/backend/utils.py
+++ multisheller/backend/utils.py
@@ -4,9 +4,9 @@ import os
 
 suffixes = {
     'cmdexe': '.bat',
-    'bash': '.sh',
-    'zsh': '.sh',
-    'xonsh': '.sh',
+    'bash': '.bash',
+    'zsh': '.zsh',
+    'xonsh': '.xsh',
     'powershell': '.ps1',
 }
 
@@ -18,12 +18,42 @@ translators = {
     'powershell': powershell,
 }
 
-def write_script(path, commands, interpreter):
+def write_script(path, commands, interpreter, filename_without_extension='script'):
     s = cmds.Script(commands)
-    fname = os.path.join(path, 'script' + suffixes[interpreter])
+    fname = os.path.join(path, filename_without_extension + suffixes[interpreter])
 
     with open(fname, 'w') as f:
         s = translators[interpreter].to_script(s)
         f.write(s)
 
     return fname
+
+# See https://github.com/mamba-org/multisheller/issues/11
+def write_bash_zsh_disambiguation_script(path, filename_without_extension='script'):
+    fname = os.path.join(path, filename_without_extension + '.sh')
+
+    with open(fname, 'w') as f:
+        f.write(r'if [ ! -z "$ZSH_VERSION" ]; then')
+        f.write('\n')
+        f.write(r'  SCRIPT_DIR=${0:a:h}')
+        f.write('\n')
+        f.write(r'  SCRIPT_NAME=$(basename "${(%):-%x}")')
+        f.write('\n')
+        f.write(r'  SCRIPT_NAME_WITHOUT_EXT=${SCRIPT_NAME%.*}')
+        f.write('\n')
+        f.write(r'  source ${SCRIPT_DIR}/${SCRIPT_NAME_WITHOUT_EXT}.zsh')
+        f.write('\n')
+        f.write("else\n")
+        f.write(r'  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"')
+        f.write('\n')
+        f.write(r'  SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")')
+        f.write('\n')
+        f.write(r'  SCRIPT_NAME_WITHOUT_EXT=${SCRIPT_NAME%.*}')
+        f.write('\n')
+        f.write(r'  source ${SCRIPT_DIR}/${SCRIPT_NAME_WITHOUT_EXT}.bash')
+        f.write('\n')
+        f.write('fi\n')
+
+    f.close()
+
+    return
